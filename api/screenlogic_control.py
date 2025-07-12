76af83928942da9f1d902c7aae2f1f11464bbcc6
druvolo1 <104272483+druvolo1@users.noreply.github.com>
2025-07-12 12:26:09 -0400

    1

--- api/screenlogic_control.py
+++ api/screenlogic_control.py
@@ -34,6 +34,8 @@ def _gw_host() -> str:
 @bp.route("/control", methods=["POST"])
 def control():
     data = request.get_json(force=True) or {}
+    _log.debug(f"Raw content type: {request.content_type}")
+    _log.debug(f"Raw request body: {request.data}")
     _log.debug(f"Received payload: {data}")
     target = data.get("target")
 
